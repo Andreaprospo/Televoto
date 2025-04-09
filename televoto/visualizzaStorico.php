a86f3a03208a9e2a44e87e349c8f70a6b9f69017
Momenskrrrt <omardouaifia20@gmail.com>
2025-04-09 19:48:48 +0200

    Ho effettivamente fatto lo storico
    mi ero dimenticato completamente dello storico

--- televoto/visualizzaStorico.php
+++ televoto/visualizzaStorico.php
@@ -1,4 +1,5 @@
 <?php
+require_once("conn.php");
 if(!isset($_SESSION))
 {
     session_start();
@@ -27,6 +28,32 @@ if($_SESSION["privilegio"] != "P" || $_SESSION["privilegio"] != "P+A" )
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    $sql = "SELECT v.domanda, r.risposta FROM votazioni v JOIN risposte r ON v.IDvotazione = r.IDvotazione WHERE v.IDvotazione = ( SELECT IDvotazione FROM votazioni ORDER BY IDcollegio DESC LIMIT 1);";
+    $result = $mysqli->query($sql);
+    if($result->num_rows > 0)
+    {
+        $currentQuestion = "";
+        echo "<ul>";
+        while($row = $result->fetch_assoc())
+        {
+            if ($currentQuestion != $row["domanda"]) {
+                if ($currentQuestion != "") {
+                    echo "</ul>";
+                }
+                $currentQuestion = $row["domanda"];
+                echo "<h1>".$currentQuestion."</h1>";
+                echo "<h2>Risposte:</h2>";
+                echo "<ul>";
+            }
+            echo "<li>".$row["risposta"]."</li>";
+        }
+        echo "</ul>";
+    }
+    else
+    {
+        echo "Nessuna votazione trovata.";
+    }
+    ?>
 </body>
 </html>
