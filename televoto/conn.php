3ae45444b39a7db9ed0cd3ccc81b8c29bc092332
Andreaprospo <proserpioandrea2006@gmail.com>
2025-04-10 17:10:10 +0200

    nuovo codice

--- televoto/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "televoto");
-
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-    exit();
-}
-?>
